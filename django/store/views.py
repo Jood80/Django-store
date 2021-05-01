c3b83709c25f31f46f132c6bc6dccad52d233ad4
Nujood <shawar.nujood@gmail.com>
2021-05-01 16:37:44 +0300

    add product serializer for viewing products list

--- django/store/views.py
+++ django/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Product, Category
+from .serializers import ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
